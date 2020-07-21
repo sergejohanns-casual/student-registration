dc9ecd55fe91851fb491a33aca346502b2456d26
SergeJohanns <serge.johanns@gmail.com>
2020-07-21 20:30:56 +0200

    Changed API endpoints

--- flaskr/student_registration.py
+++ flaskr/student_registration.py
@@ -46,22 +46,11 @@ db.create_all()
 def landing_page():
     return app.send_static_file("index.html")
 
-@app.route(API_PATH + "activities/", methods=["GET", "POST"])
-def activities():
-    if request.method == "POST":
-        return "I don't know how to make activities"
-    else:
-        return "activities"
 
-@app.route(API_PATH + "activities/<int:id>", methods=["GET", "PUT", "DELETE"])
-def activity(id):
-    if request.method == "PUT":
-        return "I don't know how to change activities"
-    elif request.method == "DELETE":
-        return "I don't know how to delete activities"
-    else:
-        return "activity"
-
-@app.route(API_PATH + "enroll")
+@app.route(API_PATH + "enroll/")
 def enroll():
     return "RIP"
+
+@app.route(API_PATH + "enroll/<key>")
+def confirm_enroll():
+    return "I don't know how to confirm enrollment"
