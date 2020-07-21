67bdc2315423e9b1157ddf0c46680607cdbcc69a
SergeJohanns <serge.johanns@gmail.com>
2020-07-22 01:01:03 +0200

    Implemented database interaction

--- flaskr/student_registration.py
+++ flaskr/student_registration.py
@@ -1,5 +1,7 @@
+import secrets
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import BadRequest
 
 
 app = Flask(
@@ -30,6 +32,12 @@ class User(db.Model):
         return "<Student {}>".format(self.id)
 
 
+class TempEnrollment(db.Model):
+    id = db.Column(db.Text, primary_key=True)
+    activity_id = db.Column(db.Integer, db.ForeignKey("activity.id"))
+    user_id = db.Column(db.Text, db.ForeignKey("user.id"))
+
+
 class Enrollment(db.Model):
     id = db.Column(db.Text, primary_key=True)
     activity_id = db.Column(db.Integer, db.ForeignKey("activity.id"))
@@ -42,16 +50,33 @@ class Enrollment(db.Model):
 db.create_all()
 
 
+def gen_key(user, activity):
+    while True:
+        res = secrets.token_urlsafe(32)
+        if Enrollment.query.filter_by(id=res).first() is None and TempEnrollment.query.filter_by(id=res).first() is None:
+            return res
+
+
 @app.route("/")
-def landing_page():
+def index_page():
     return app.send_static_file("index.html")
 
 
 @app.route(API_PATH + "enroll/", methods=["POST"])
 def enroll():
-    pass
+    data = request.json
+    if data is None or "id" not in data or "activity" not in data:
+        raise BadRequest
+    uid, aid = data["id"], data["activity"]
+    user = User.query.filter_by(id=uid).first_or_404()
+    activity = Activity.query.filter_by(id=aid).first_or_404()
+    db.session.add(TempEnrollment(id=gen_key(user, activity), user_id=user.id, activity_id=activity.id))
+    db.session.commit()
 
 
 @app.route(API_PATH + "enroll/<key>", methods=["POST"])
 def confirm_enroll():
-    return "I don't know how to confirm enrollment"
+    temp = TempEnrollment.query.filter_by(id=request.args.get("key")).first_or_404()
+    db.session.remove(temp)
+    db.session.add(Enrollment(id=temp.id, user_id=temp.user_id, activity_id=temp.activity_id))
+    db.session.commit()
