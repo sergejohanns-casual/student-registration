3bf830386b6ba99f65f44f5707b6aa725bb3a3bc
SergeJohanns <serge.johanns@gmail.com>
2020-07-28 18:45:14 +0200

    Finished email functionality

--- flaskr/student_registration.py
+++ flaskr/student_registration.py
@@ -1,6 +1,9 @@
 import json
 import http
+import string
 import secrets
+
+import yagmail
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import BadRequest, UnprocessableEntity
@@ -16,11 +19,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
 API_PATH = "/api/"
+CONFIRM_PAGE = string.Template("/enroll?key=${key}")
+
+# Email
+TEMPLATE_FILE = "template.html"
+with open(TEMPLATE_FILE, 'r') as template_file:
+    TEMPLATE = string.Template(template_file.read())
+SUBJECT = "Enrollment confirmation"
 CREDENTIALS_FILE = "credentials.json"
 with open(CREDENTIALS_FILE, 'r') as cred_file:
     data = json.load(cred_file)
-    EMAIL_CLIENT = data["client"]
-    EMAIL_PORT = data["port"]
     SENDER_ADDRESS = data["address"]
     SENDER_PASSWORD = data["password"]
 
@@ -57,6 +65,16 @@ def gen_key(user, activity):
             return res
 
 
+def send_mail(email, key, activity):
+    with yagmail.SMTP(user=SENDER_ADDRESS, password=SENDER_PASSWORD) as smtp:
+        smtp.send(
+            to=email,
+            subject=SUBJECT,
+            contents=[TEMPLATE.substitute(
+                activity=activity,
+                confirm_page=request.url_root + CONFIRM_PAGE.substitute(key=key))])
+
+
 @app.route("/")
 def index_page():
     return app.send_static_file("index.html")
@@ -74,8 +92,10 @@ def enroll():
     activity = Activity.query.filter_by(id=aid).first()
     if user is None or activity is None:
         raise UnprocessableEntity
-    db.session.add(TempEnrollment(id=gen_key(user, activity), user_id=user.id, activity_id=activity.id))
+    key=gen_key(user, activity)
+    db.session.add(TempEnrollment(id=key, user_id=user.id, activity_id=activity.id))
     db.session.commit()
+    send_mail(user.email, key, activity.name)
     return '', http.HTTPStatus.NO_CONTENT
 
 
