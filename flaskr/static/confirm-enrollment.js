d0bf23ed73d543678398244e3631ab46529e6832
SergeJohanns <serge.johanns@gmail.com>
2020-07-30 01:56:33 +0200

    Implemented ticket serving

--- flaskr/static/confirm-enrollment.js
+++ flaskr/static/confirm-enrollment.js
@@ -8,8 +8,9 @@ if (urlParams.has("key")) {
     request.open("POST", API_CONFIRM_ENROLL + urlParams.get("key"));
     request.onreadystatechange = function() {
         if (this.readyState === XMLHttpRequest.DONE) {
-            if (this.status === 204) {
-                textField.innerHTML = "Your enrollment was confirmed succesfully.";
+            if (this.status === 204 || this.status === 409) {
+                // 409 CONFLICT means the confirmation was already done, so we just repeat the success message.
+                textField.innerHTML = "Your enrollment was confirmed succesfully. You have been sent an email with your ticket.";
             } else {
                 console.log("Unexpected response code: " + this.status);
                 textField.innerHTML = "An error occured while confirming your enrollment.";
--- flaskr/student_registration.py
+++ flaskr/student_registration.py
@@ -6,7 +6,7 @@ import secrets
 import yagmail
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.exceptions import BadRequest, UnprocessableEntity
+from werkzeug.exceptions import BadRequest, UnprocessableEntity, Conflict
 
 
 app = Flask(
@@ -20,12 +20,18 @@ db = SQLAlchemy(app)
 
 API_PATH = "/api/"
 CONFIRM_PAGE = string.Template("/enroll?key=${key}")
+ID_LENGTH = 32
+TICKET_LENGTH = 16
 
 # Email
-TEMPLATE_FILE = "template.html"
-with open(TEMPLATE_FILE, 'r') as template_file:
-    TEMPLATE = string.Template(template_file.read())
-SUBJECT = "Enrollment confirmation"
+CONFIRM_TEMPLATE_FILE = "email-templates/confirm-template.html"
+with open(CONFIRM_TEMPLATE_FILE, 'r') as template_file:
+    CONFIRM_TEMPLATE = string.Template(template_file.read())
+CONFIRM_SUBJECT = "Enrollment confirmation"
+TICKET_TEMPLATE_FILE = "email-templates/ticket-template.html"
+with open(TICKET_TEMPLATE_FILE, 'r') as template_file:
+    TICKET_TEMPLATE = string.Template(template_file.read())
+TICKET_SUBJECT = "Enrollment ticket"
 CREDENTIALS_FILE = "credentials.json"
 with open(CREDENTIALS_FILE, 'r') as cred_file:
     data = json.load(cred_file)
@@ -43,14 +49,9 @@ class User(db.Model):
     email = db.Column(db.Text, unique=True, nullable=False)
 
 
-class TempEnrollment(db.Model):
-    id = db.Column(db.Text, primary_key=True)
-    activity_id = db.Column(db.Integer, db.ForeignKey("activity.id"))
-    user_id = db.Column(db.Text, db.ForeignKey("user.id"))
-
-
 class Enrollment(db.Model):
-    id = db.Column(db.Text, primary_key=True)
+    id = db.Column(db.String(ID_LENGTH), primary_key=True)
+    key = db.Column(db.Text, unique=True, nullable=True, default=None) # None means not confirmed yet
     activity_id = db.Column(db.Integer, db.ForeignKey("activity.id"))
     user_id = db.Column(db.Text, db.ForeignKey("user.id"))
 
@@ -58,21 +59,37 @@ class Enrollment(db.Model):
 db.create_all()
 
 
-def gen_key(user, activity):
+def gen_id():
     while True:
-        res = secrets.token_urlsafe(32)
-        if Enrollment.query.filter_by(id=res).first() is None and TempEnrollment.query.filter_by(id=res).first() is None:
+        res = secrets.token_urlsafe(ID_LENGTH)
+        if Enrollment.query.filter_by(id=res).first() is None:
+            return res
+
+def gen_ticket():
+    while True:
+        res = secrets.token_hex(TICKET_LENGTH)
+        if Enrollment.query.filter_by(key=res).first() is None:
             return res
 
 
-def send_mail(email, key, activity):
+def confirmation_email(address, enrollment_id, activity):
     with yagmail.SMTP(user=SENDER_ADDRESS, password=SENDER_PASSWORD) as smtp:
         smtp.send(
-            to=email,
-            subject=SUBJECT,
-            contents=[TEMPLATE.substitute(
+            to=address,
+            subject=CONFIRM_SUBJECT,
+            contents=[CONFIRM_TEMPLATE.substitute(
                 activity=activity,
-                confirm_page=request.url_root + CONFIRM_PAGE.substitute(key=key))])
+                confirm_page=request.url_root + CONFIRM_PAGE.substitute(key=enrollment_id))])
+
+
+def ticket_email(address, ticket, activity):
+    with yagmail.SMTP(user=SENDER_ADDRESS, password=SENDER_PASSWORD) as smtp:
+        smtp.send(
+            to=address,
+            subject=TICKET_SUBJECT,
+            contents=[TICKET_TEMPLATE.substitute(
+                activity=activity,
+                ticket=ticket)])
 
 
 @app.route("/")
@@ -97,17 +114,25 @@ def enroll():
     activity = Activity.query.filter_by(id=aid).first()
     if user is None or activity is None:
         raise UnprocessableEntity
-    key=gen_key(user, activity)
-    db.session.add(TempEnrollment(id=key, user_id=user.id, activity_id=activity.id))
+    enrollment_id=gen_id()
+    db.session.add(Enrollment(
+        id=enrollment_id,
+        user_id=user.id,
+        activity_id=activity.id))
     db.session.commit()
-    send_mail(user.email, key, activity.name)
+    confirmation_email(user.email, enrollment_id, activity.name)
     return '', http.HTTPStatus.NO_CONTENT
 
 
 @app.route(API_PATH + "enroll/<key>", methods=["POST"])
 def confirm_enroll(key):
-    temp = TempEnrollment.query.filter_by(id=key).first_or_404()
-    db.session.delete(temp)
-    db.session.add(Enrollment(id=temp.id, user_id=temp.user_id, activity_id=temp.activity_id))
+    enrollment = Enrollment.query.filter_by(id=key).first_or_404()
+    if enrollment.key is not None:
+        raise Conflict
+    key = gen_ticket()
+    enrollment.key = key
     db.session.commit()
-    return '', http.HTTPStatus.NO_CONTENT
+    email = User.query.filter_by(id=enrollment.user_id).one().email
+    activity = Activity.query.filter_by(id=enrollment.activity_id).one().name
+    ticket_email(address=email, ticket=key, activity=activity)
+    return "", http.HTTPStatus.NO_CONTENT
